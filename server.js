7b19c2a3ed959ab0c7e5a12c23de522b110e5942
Pavel Salianik <solyqnik@gmail.com>
2023-05-21 20:46:32 +0300

    сделал модальное окно

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-const path = require("path")
-const express = require("express")
-const webpack = require("webpack")
-const webpackMiddleware = require("webpack-dev-middleware")
-const webpackConfig = require("./webpack.config")
-const bodyParser = require("body-parser")
-const uuid = require("uuid")
+const path = require("path");
+const express = require("express");
+const webpack = require("webpack");
+const webpackMiddleware = require("webpack-dev-middleware");
+const webpackConfig = require("./webpack.config");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
 
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(webpackMiddleware(webpack(webpackConfig)))
-const publicPath = path.join(__dirname, "views")
-const port = process.env.PORT || 9000
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(webpackMiddleware(webpack(webpackConfig)));
+const publicPath = path.join(__dirname, "views");
+const port = process.env.PORT || 9000;
 
-const todos = [
+const todos = [];
+let isAuth = false;
 
-]
+app.get("/", (req, res) => {
+  res.render("index", {
+    isAuth,
+  });
+});
 
-app.get('/', (req, res) => {
-  res.render("index")
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    isAuth,
+  });
+});
 
-app.get('/about', (req, res) => {
-  res.render("about")
-})
-
-app.get('/todo-list', (req, res) => {
+app.get("/todo-list", (req, res) => {
   res.render("todo-list", {
-    todos
-  })
-})
+    todos,
+    isAuth,
+  });
+});
 
-app.post('/create-todo', (req, res) => {
-  const body = req.body
-  todos.push({ ...body, id: uuid.v4() })
+app.get("/login", (req, res) => {
+  res.render("turbo/modals/login");
+});
+
+app.post("/create-todo", (req, res) => {
+  const body = req.body;
+  todos.push({ ...body, id: uuid.v4() });
   res.render("todo-list", {
-    todos
-  })
-})
+    todos,
+    isAuth,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
-
+  console.log(`Listening on port ${port}`);
+});
 
 // $env:NODE_OPTIONS = "--openssl-legacy-provider"
