9f15cf2e868e9107b138cd1cfa5a646649c4ed49
Pavel Salianik <solyqnik@gmail.com>
2023-05-23 16:45:07 +0300

    test turbo-stream

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const webpack = require("webpack");
 const webpackMiddleware = require("webpack-dev-middleware");
 const webpackConfig = require("./webpack.config");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
-const session = require('express-session')
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const uuid = require("uuid");
 
 const app = express();
@@ -15,19 +15,19 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(webpackMiddleware(webpack(webpackConfig)));
-app.use(cookieParser('secret key'));
+app.use(cookieParser("secret key"));
 app.use(
   session({
-      secret: 'you secret key',
-      saveUninitialized: true,
+    secret: "you secret key",
+    saveUninitialized: true,
   })
 );
 
 app.use((err, req, res, next) => {
-  console.log(req.session.isSecondLoad)
-  req.session.isSecondLoad = true
-  next()
-})
+  console.log(req.session.isSecondLoad);
+  req.session.isSecondLoad = true;
+  next();
+});
 
 const publicPath = path.join(__dirname, "views");
 const port = process.env.PORT || 9000;
@@ -49,7 +49,7 @@ app.get("/about", (req, res) => {
 
 app.get("/todo-list", (req, res) => {
   if (!isAuth) {
-    return res.redirect("/login")
+    return res.redirect("/login");
   }
   res.render("todo-list", {
     todos,
@@ -68,6 +68,22 @@ app.post("/create-todo", (req, res) => {
     todos,
     isAuth,
   });
+});
+
+let count = 0;
+
+app.get("/update", (req, res) => {
+  count++;
+  // const content = `
+  // <turbo-stream action="update" target="counter">
+  //   <template>
+  //     <div id="counter">${count}</div>
+  //   <template>
+  // </turbo-stream>
+  // `;
+  // res.setHeader("Content-Type", "text/vnd.turbo-stream.html; charset=utf-8");
+  // res.send(content);
+  res.render("turbo/html.ejs");
 });
 
 app.listen(port, () => {
